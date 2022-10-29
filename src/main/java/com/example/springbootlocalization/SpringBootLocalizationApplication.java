1c2bef0c7eff9588692e18c4fb8d92f5c94fc869
Khode Me <phearakroun@gmail.com>
2022-10-29 15:12:49 +0700

    Spring Localization

---
+++ src/main/java/com/example/springbootlocalization/SpringBootLocalizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlocalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLocalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLocalizationApplication.class, args);
+    }
+
+}
